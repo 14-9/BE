cafe2c4eaa24318d4ecf9a6c282f925caa1e17db
mansooonuna <parkdasom95@gmail.com>
2023-05-06 00:14:13 +0900

    Fix : username으로 검색하고 있던 것 memberId로 변경

--- src/main/java/com/example/weluvwine/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/weluvwine/security/UserDetailsServiceImpl.java
@@ -1,4 +1,7 @@
+package com.example.weluvwine.security;
+
 import com.example.weluvwine.member.entity.Member;
+import com.example.weluvwine.member.repository.MemberRepository;
 import com.example.weluvwine.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,13 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final MemberRepository userRepository;
+    private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = userRepository.findByUsername(username)
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Member member = memberRepository.findMemberByMemberId(memberId)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return new UserDetailsImpl(member, member.getUsername());
+        return new UserDetailsImpl(member, member.getMemberId());
     }
 }
