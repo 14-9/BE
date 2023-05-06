fe5929ead43c68ae5d9f81a05f172cc2ea0af22d
been1118 <dsin1118@kakao.com>
2023-05-07 01:29:52 +0900

    Refactor : Controller Swagger 메서드 설명 추가

--- src/main/java/com/example/weluvwine/domain/recommend/controller/RecommendController.java
+++ src/main/java/com/example/weluvwine/domain/recommend/controller/RecommendController.java
@@ -2,6 +2,7 @@ package com.example.weluvwine.domain.recommend.controller;
 
 import com.example.weluvwine.domain.recommend.service.RecommendService;
 import com.example.weluvwine.security.auth.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ public class RecommendController {
 
     private final RecommendService recommendService;
 
+    @Operation(summary = "추천하기 메서드", description = "추천하기 메서드입니다.")
     @PostMapping("/api/recommend/{wine-id}")
     public ResponseEntity recommendWine(@PathVariable(name = "wine-id") Long wineId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return recommendService.recommendWine(wineId, userDetails.getMember());
--- src/main/java/com/example/weluvwine/domain/wine/controller/WineController.java
+++ src/main/java/com/example/weluvwine/domain/wine/controller/WineController.java
@@ -3,6 +3,7 @@ package com.example.weluvwine.domain.wine.controller;
 import com.example.weluvwine.domain.wine.service.WineService;
 import com.example.weluvwine.security.auth.UserDetailsImpl;
 import com.example.weluvwine.util.Message;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class WineController {
     private final WineService wineService;
 
+    @Operation(summary = "검색 메서드", description = "검색 메서드입니다.")
     @GetMapping("/api/search")
     public ResponseEntity<Message> searchWine(@RequestParam String searchKeyword, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return wineService.searchWine(searchKeyword);
     }
+    @Operation(summary = "와인상세조회 메서드", description = "와인상세조회 메서드입니다.")
     @GetMapping("/api/review/{review-id}")
     public ResponseEntity<Message> readWine(@PathVariable(name = "review-id") Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return wineService.readWine(reviewId);
     }
+    @Operation(summary = "와인랭킹 조회 메서드", description = "와인랭킹 조회 메서드입니다.")
     @GetMapping("/api/rank")
     public ResponseEntity<Message> rankWine(){
         return wineService.rankWine();
