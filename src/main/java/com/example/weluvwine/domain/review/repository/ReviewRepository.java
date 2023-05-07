d2ab282d7321a6ba1ac82f1cc592348f9de130cc
Dunkin00 <meister630@gmail.com>
2023-05-07 22:58:43 +0900

    Fix : 마이페이지 목록 조회시 오류 해결
    마이페이지 목록 조회시 findById -> findByMemberId로 변경

--- src/main/java/com/example/weluvwine/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/weluvwine/domain/review/repository/ReviewRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findAllById(Long memberId);
+    List<Review> findAllByMemberId(Long memberId);
 }
