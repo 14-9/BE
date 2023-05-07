a7b6bd1e96def13e1296a04ff0ea4986ab5e7477
been1118 <dsin1118@kakao.com>
2023-05-08 00:43:30 +0900

    Refactor : logout Method Get -> Post 변경

--- src/main/java/com/example/weluvwine/domain/member/controller/MemberController.java
+++ src/main/java/com/example/weluvwine/domain/member/controller/MemberController.java
@@ -37,7 +37,7 @@ public class MemberController {
     }
 
     @Operation(summary = "로그아웃 메서드", description = "로그아웃 메서드입니다.")
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public ResponseEntity<Message> logout(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request) {
         return memberService.logout(userDetails.getMember(), request);
     }
