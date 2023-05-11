6c770aa09f55cfd4c2fae06e28e3af403c13793a
been1118 <dsin1118@kakao.com>
2023-05-11 15:38:31 +0900

    Fix : ReviewRequestDto @NoArgsConstructor 추가

--- src/main/java/com/example/weluvwine/domain/review/dto/ReviewRequestDto.java
+++ src/main/java/com/example/weluvwine/domain/review/dto/ReviewRequestDto.java
@@ -1,8 +1,12 @@
 package com.example.weluvwine.domain.review.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewRequestDto {
     private String content;
 }
