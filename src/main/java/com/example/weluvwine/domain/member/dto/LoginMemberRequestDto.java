09b550ae9cddb8e327617f415dcfcecf6488527c
been1118 <dsin1118@kakao.com>
2023-05-11 15:38:08 +0900

    Fix : LoginMemberRequestDto @NoArgsConstructor 추가

--- src/main/java/com/example/weluvwine/domain/member/dto/LoginMemberRequestDto.java
+++ src/main/java/com/example/weluvwine/domain/member/dto/LoginMemberRequestDto.java
@@ -2,9 +2,11 @@ package com.example.weluvwine.domain.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginMemberRequestDto {
     private String memberId;
     private String password;
